dac7411b471ffcd49edf2a7433278b6662120d5e
minseob <leeminseob@outlook.com>
2020-03-10 07:07:04 +0900

    Add FloatVector3 operators

--- include/LEFloatVector3.h
+++ include/LEFloatVector3.h
@@ -3,6 +3,8 @@
 // @brief 3Dimensions Float Vector 
 // @author minseob
 #pragma once
+#include <math.h>
+
 #include "LEMathDataContainer.h"
 
 namespace LEMath {
@@ -24,8 +26,32 @@ namespace LEMath {
         inline float X() const { return x; }
         inline float Y() const { return y; }
         inline float Z() const { return z; }
+        float Length() const { return ::sqrtf(x * x + y * y + z * z); }
+        FloatVector3& Normalize() {
+            float length = Length();
+            if (length > 1e-4) {
+                x = x / length;
+                y = y / length;
+                z = z / length;
+            }
+            return *this;
+        }
+        FloatVector3 Minimum(const FloatVector3 &In) {
+            return FloatVector3((x < In.X()) ? x : In.X(), (y < In.Y()) ? y : In.Y(), (z < In.Z()) ? z : In.Z());
+        }
+        FloatVector3 Maximum(const FloatVector3 &In) {
+            return FloatVector3((x > In.X()) ? x : In.X(), (y > In.Y()) ? y : In.Y(), (z > In.Z()) ? z : In.Z());
+        }
 
-		// Set
+        // Caclulator (static)
+        static FloatVector3 CrossProduct(const FloatVector3 &A, const FloatVector3 &B) {
+            return FloatVector3(A.Y() * B.Z() - A.Z() * B.Y(), A.Z() * B.X() - A.X() * B.Z(), A.X() * B.Y() - A.Y() * B.X(), 0.0f);
+        }
+        static float DotProduct(const FloatVector3 &A, const FloatVector3 &B) {
+            return A.X() * B.X() + A.Y() * B.Y() + A.Z() * B.Z();
+        }
+        
+        // Set
         FloatVector3& SetX(float X) { x = X; return *this; }
         FloatVector3& SetY(float Y) { y = Y; return *this; }
         FloatVector3& SetZ(float Z) { z = Z; return *this; }
@@ -54,10 +80,57 @@ namespace LEMath {
 
 		// Operators
 		bool operator == (const FloatVector3 &In) const { In.X() == x && In.Y() == y && In.Z() == z; }
+        FloatVector3 operator + (const FloatVector3 &In) const {
+            return FloatVector3(x + In.X(), y + In.Y(), z + In.Z());
+        }
+        FloatVector3& operator += (const FloatVector3 &In) {
+            x += In.X();
+            y += In.Y();
+            z += In.Z();
+            return *this;
+        }
+        FloatVector3 operator - (const FloatVector3 &In) const {
+            return FloatVector3(x - In.X(), y - In.Y(), z - In.Z());
+        }
+        FloatVector3& operator -= (const FloatVector3 &In) {
+            x -= In.X();
+            y -= In.Y();
+            z -= In.Z();
+            return *this;
+        }
+        FloatVector3 operator * (const FloatVector3 &In) const {
+            return FloatVector3(x * In.X(), y * In.Y(), z * In.Z());
+        }
+        FloatVector3& operator *= (const FloatVector3 &In) {
+            x *= In.X();
+            y *= In.Y();
+            z *= In.Z();
+            return *this;
+        }
+        FloatVector3 operator / (const FloatVector3 &In) const {
+            return FloatVector3(x / In.X(), y + In.Y(), z / In.Z());
+        }
+        FloatVector3& operator /= (const FloatVector3 &In) {
+            x /= In.X();
+            y /= In.Y();
+            z /= In.Z();
+            return *this;
+        }
+        FloatVector3 operator ^ (const FloatVector3 &In) const {
+            return CrossProduct(*this, In);
+        }
+        bool operator < (const FloatVector3 &In) const {
+            return x < In.X() && y < In.Y() && z < In.Z();
+        }
+        bool operator > (const FloatVector3 &In) const {
+            return x > In.X() && y > In.Y() && z > In.Z();
+        }
         operator DataContainer() const {
             DataContainer output;
 			return GenerateDataContainer(output);
         }
+        static const FloatVector3 Zero;
+        static const FloatVector3 One;
 	private:
 		DataContainer& GenerateDataContainer(DataContainer &Output) const {
             Output.Type = DataContainer::DataType::Float;
--- source/LEFloatVector3.cpp
+++ source/LEFloatVector3.cpp
@@ -5,6 +5,8 @@
 #include "LEFloatVector3.h"
 
 namespace LEMath {
+    static const FloatVector3::Zero = FloatVector3(0.0f, 0.0f, 0.0f);
+    static const FloatVector3::One  = FloatVector3(1.0f, 1.0f, 1.0f);
     bool DataContainer::ToFloatVector3(FloatVector3 *Output) const {
         Output->SetX(GetFloatValue(0));
         Output->SetY(GetFloatValue(1));
